df71a3da93a8a93406bed5273db6ac544dd8ee7a
manuelduarte077 <carlosd99m@gmail.com>
2022-10-12 00:42:56 -0600

    feat: task card

---
+++ lib/features/task/tasks_card_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class TasksCardScreen extends StatelessWidget {
+  const TasksCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            const WidgetNotes(
+              title: '✍️ Youtube script ideas',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            /// Separator
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+
+            const WidgetNotes(
+              title: '📚 Read a book',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            /// Separator
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+
+            const WidgetNotes(
+              title: '📝 Write a blog post',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            /// Separator
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+
+            const WidgetNotes(
+              title: '📘 Datastore Blog',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            /// Separator
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+
+            const WidgetNotes(
+              title: '🎭 College Skit review',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            /// Separator
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+
+            const WidgetNotes(
+              title: '📌 Points to remember',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            /// Separator
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+
+            const WidgetNotes(
+              title: 'College skit review 🏄😝',
+              description:
+                  'There are many apps in Android that can run or emulate other operating systems, via utilizing hardware support for platform... ',
+            ),
+
+            SizedBox(
+              height: 0.5,
+              child: Container(
+                color: const Color(0xFFBDBDBD),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WidgetNotes extends StatelessWidget {
+  const WidgetNotes({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      subtitle: Text(
+        description,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+}
